9031960d19cbd6a7338dc0630a00df52cc149029
Miguel Angel Jimenez Fajardo <128429185+FARNIKS@users.noreply.github.com>
2025-02-10 20:14:39 -0500

    -Se agrega funcion de urls para la musica q sea dinamico, se elimina boton de mas informacion y se arreglaron los bugs, solo queda agregar la informacion

--- src/main/java/com/farniks/appfoneticaipa/controller/InformationExampleIpaController.java
+++ src/main/java/com/farniks/appfoneticaipa/controller/InformationExampleIpaController.java
@@ -14,7 +14,6 @@ import java.net.URISyntaxException;
 public class InformationExampleIpaController {
     @FXML
     Button btnHome, btnMenu, btnMode, btnGithub,
-            btnMoreInformation,
             btnSentenceSound,
             btnExampleSound1, btnExampleSound2, btnExampleSound3, btnExampleSound4, btnExampleSound5, btnExampleSound6,
             btnSymbolSound;
@@ -25,15 +24,7 @@ public class InformationExampleIpaController {
 
     private SoundsPhonetics soundsPhonetics;
 
-    // Atributos para las rutas de los archivos de audio
-    private String symbolSound;
-    private String sentenceSound;
-    private String exampleSound1;
-    private String exampleSound2;
-    private String exampleSound3;
-    private String exampleSound4;
-    private String exampleSound5;
-    private String exampleSound6;
+    private String audioSymbol, audioSentence, audioExample1, audioExample2, audioExample3, audioExample4, audioExample5, audioExample6, urlPage;
 
     public InformationExampleIpaController() {
 
@@ -46,7 +37,7 @@ public class InformationExampleIpaController {
         }else if (sourceButton == btnMode) {
             System.out.println("Mode");
         }else if (sourceButton == btnMenu) {
-            System.out.println();
+            changePage.loadStage(urlPage, mouseEvent, "Vocales");
         }else if (sourceButton == btnGithub){
             try {
                 Desktop.getDesktop().browse(new URI("https://github.com/FARNIKS/APP-Fonetica-IPA"));
@@ -64,26 +55,43 @@ public class InformationExampleIpaController {
         String musicFile = "";
 
         if (sourceButton == btnSymbolSound) {
-            musicFile = symbolSound;
+            musicFile = audioSymbol;
         } else if (sourceButton == btnSentenceSound) {
-            musicFile = sentenceSound;
+            musicFile = audioSentence;
         } else if (sourceButton == btnExampleSound1) {
-            musicFile = exampleSound1;
+            musicFile = audioExample1;
         } else if (sourceButton == btnExampleSound2) {
-            musicFile = exampleSound2;
+            musicFile = audioExample2;
         } else if (sourceButton == btnExampleSound3) {
-            musicFile = exampleSound3;
+            musicFile = audioExample3;
         } else if (sourceButton == btnExampleSound4) {
-            musicFile = exampleSound4;
+            musicFile = audioExample4;
         } else if (sourceButton == btnExampleSound5) {
-            musicFile = exampleSound5;
+            musicFile = audioExample5;
         } else if (sourceButton == btnExampleSound6) {
-            musicFile = exampleSound6;
+            musicFile = audioExample6;
         }
-        soundsPhonetics = new SoundsPhonetics(musicFile);
+
+        if (!musicFile.isEmpty()) {
+            soundsPhonetics = new SoundsPhonetics(musicFile);
+        }
     }
 
     public void eventButtonInformation(MouseEvent mouseEvent) {
+    }
+
+    public void setAudioFiles(String symbolAudio, String sentenceAudio, String example1Audio,
+                              String example2Audio, String example3Audio, String example4Audio,
+                              String example5Audio, String example6Audio, String urlPage) {
+        this.audioSymbol = symbolAudio;
+        this.audioSentence = sentenceAudio;
+        this.audioExample1 = example1Audio;
+        this.audioExample2 = example2Audio;
+        this.audioExample3 = example3Audio;
+        this.audioExample4 = example4Audio;
+        this.audioExample5 = example5Audio;
+        this.audioExample6 = example6Audio;
+        this.urlPage = urlPage;
     }
 
     public void setInformation(
@@ -115,39 +123,5 @@ public class InformationExampleIpaController {
             lbSymbol.setText(symbol);
         }
     }
-
-    // Métodos setters para las rutas de los sonidos
-    public void setSymbolSound(String path) {
-        this.symbolSound = path;
-    }
-
-    public void setSentenceSound(String path) {
-        this.sentenceSound = path;
-    }
-
-    public void setExampleSound1(String path) {
-        this.exampleSound1 = path;
-    }
-
-    public void setExampleSound2(String path) {
-        this.exampleSound2 = path;
-    }
-
-    public void setExampleSound3(String path) {
-        this.exampleSound3 = path;
-    }
-
-    public void setExampleSound4(String path) {
-        this.exampleSound4 = path;
-    }
-
-    public void setExampleSound5(String path) {
-        this.exampleSound5 = path;
-    }
-
-    public void setExampleSound6(String path) {
-        this.exampleSound6 = path;
-    }
-
 
 }
--- src/main/java/com/farniks/appfoneticaipa/controller/VowelsMonophthongsController.java
+++ src/main/java/com/farniks/appfoneticaipa/controller/VowelsMonophthongsController.java
@@ -87,146 +87,252 @@ public class VowelsMonophthongsController{
         Button sourceButton = (Button) mouseEvent.getSource();
         // Variables por defecto
         String symbol = "iː";
+        String soundSymbol = "/media/Vowels/Vocal1.mp3";
         String sentence = "She needs a new seat";
+        String soundSentence = "/media/Vowels/iː/She needs a new seat.mp3";
         String example1 = "Feet - /fiːt/";
+        String soundExample1 = "/media/Vowels/iː/Feet.mp3";
         String example2 = "Eat - /iːt/";
+        String soundExample2 = "/media/Vowels/iː/Eat.mp3";
         String example3 = "Leave - /liːv/";
+        String soundExample3 = "/media/Vowels/iː/Leave.mp3";
         String example4 = "Green - /ɡriːn/";
+        String soundExample4 = "/media/Vowels/iː/Green.mp3";
         String example5 = "Tree - /triː/";
+        String soundExample5 = "/media/Vowels/iː/Tree.mp3";
         String example6 = "See - /siː/";
-
+        String soundExample6 = "/media/Vowels/iː/See.mp3";
+        String selectedUrlPage = "vowels-monophthongs.fxml";
 
         if (sourceButton == btnMoreInformation1) {
             symbol = "iː";
+            soundSymbol = "/media/Vowels/Vocal1.mp3";
             sentence = "She needs a new seat";
+            soundSentence = "/media/Vowels/iː/She needs a new seat.mp3";
             example1 = "Feet - /fiːt/";
+            soundExample1 = "/media/Vowels/iː/Feet.mp3";
             example2 = "Eat - /iːt/";
+            soundExample2 = "/media/Vowels/iː/Eat.mp3";
             example3 = "Leave - /liːv/";
+            soundExample3 = "/media/Vowels/iː/Leave.mp3";
             example4 = "Green - /ɡriːn/";
+            soundExample4 = "/media/Vowels/iː/Green.mp3";
             example5 = "Tree - /triː/";
+            soundExample5 = "/media/Vowels/iː/Tree.mp3";
             example6 = "See - /siː/";
+            soundExample6 = "/media/Vowels/iː/See.mp3";
 
 
 
         }else if (sourceButton == btnMoreInformation2) {
             symbol = "ɪ";
+            soundSymbol = "/media/Vowels/Vocal2.mp3";
             sentence = "This is a big fish";
+            soundSentence = "/media/Vowels/ɪ/This is a big fish.mp3";
             example1 = "Big - /bɪɡ/";
+            soundExample1 = "/media/Vowels/ɪ/Big.mp3";
             example2 = "Fish - /fɪʃ/";
+            soundExample2 = "/media/Vowels/ɪ/Fish.mp3";
             example3 = "Sit - /sɪt/";
+            soundExample3 = "/media/Vowels/ɪ/Sit.mp3";
             example4 = "Milk - /mɪlk/";
+            soundExample4 = "/media/Vowels/ɪ/Milk.mp3";
             example5 = "Tip - /tɪp/";
+            soundExample5 = "/media/Vowels/ɪ/Tip.mp3";
             example6 = "Hill - /hɪl/";
+            soundExample6 = "/media/Vowels/ɪ/Hill.mp3";
 
         }else if (sourceButton == btnMoreInformation3) {
             symbol = "ʊ";
+            soundSymbol = "/media/Vowels/Vocal3.mp3";
             sentence = "He took a book from the shelf";
-            example1 = "book - /bʊk/";
-            example2 = "look - /lʊk/";
-            example3 = "foot - /fʊt/";
-            example4 = "push - /pʊʃ/";
-            example5 = "wood - /wʊd/";
-            example6 = "good - /ɡʊd/";
+            soundSentence = "/media/Vowels/ʊ/He took a book from the shelf.mp3";
+            example1 = "Book - /bʊk/";
+            soundExample1 = "/media/Vowels/ʊ/book.mp3";
+            example2 = "Look - /lʊk/";
+            soundExample2 = "/media/Vowels/ʊ/look.mp3";
+            example3 = "Foot - /fʊt/";
+            soundExample3 = "/media/Vowels/ʊ/foot.mp3";
+            example4 = "Push - /pʊʃ/";
+            soundExample4 = "/media/Vowels/ʊ/push.mp3";
+            example5 = "Wood - /wʊd/";
+            soundExample5 = "/media/Vowels/ʊ/wood.mp3";
+            example6 = "Good - /ɡʊd/";
+            soundExample6 = "/media/Vowels/ʊ/good.mp3";
 
         }else if (sourceButton == btnMoreInformation4) {
             symbol = "uː";
+            soundSymbol = "/media/Vowels/Vocal4.mp3";
             sentence = "Luke moved to a new school";
-            example1 = "moon - /muːn/";
-            example2 = "food - /fuːd/";
-            example3 = "school - /skuːl/";
-            example4 = "room - /ruːm/";
-            example5 = "blue - /bluː/";
-            example6 = "true - /truː/";
+            soundSentence = "/media/Vowels/uː/Luke moved to a new school.mp3";
+            example1 = "Moon - /muːn/";
+            soundExample1 = "/media/Vowels/uː/moon.mp3";
+            example2 = "Food - /fuːd/";
+            soundExample2 = "/media/Vowels/uː/food.mp3";
+            example3 = "School - /skuːl/";
+            soundExample3 = "/media/Vowels/uː/school.mp3";
+            example4 = "Room - /ruːm/";
+            soundExample4 = "/media/Vowels/uː/room.mp3";
+            example5 = "Blue - /bluː/";
+            soundExample5 = "/media/Vowels/uː/blue.mp3";
+            example6 = "True - /truː/";
+            soundExample6 = "/media/Vowels/uː/true.mp3";
 
         }else if (sourceButton == btnMoreInformation5) {
             symbol = "e";
+            soundSymbol="/media/Vowels/Vocal5.mp3";
             sentence = "The red pen is on the desk";
-            example1 = "pen - /pen/";
-            example2 = "bed - /bed/";
-            example3 = "set - /set/";
-            example4 = "net - /net/";
-            example5 = "ten - /ten/";
-            example6 = "yes - /jes/";
+            soundSentence = "/media/Vowels/e/The red pen is on the desk.mp3";
+            example1 = "Pen - /pen/";
+            soundExample1 = "/media/Vowels/e/pen.mp3";
+            example2 = "Bed - /bed/";
+            soundExample2 = "/media/Vowels/e/bed.mp3";
+            example3 = "Set - /set/";
+            soundExample3 = "/media/Vowels/e/set.mp3";
+            example4 = "Net - /net/";
+            soundExample4 = "/media/Vowels/e/net.mp3";
+            example5 = "Ten - /ten/";
+            soundExample5 = "/media/Vowels/e/ten.mp3";
+            example6 = "Yes - /jes/";
+            soundExample6 = "/media/Vowels/e/yes.mp3";
 
         }else if (sourceButton == btnMoreInformation6) {
             symbol = "ɜː";
+            soundSymbol="/media/Vowels/Vocal6.mp3";
             sentence = "The bird heard a word";
-            example1 = "bird - /bɜːd/";
-            example2 = "word - /wɜːd/";
-            example3 = "third - /θɜːd/";
-            example4 = "learn - /lɜːn/";
-            example5 = "nurse - /nɜːs/";
-            example6 = "early - /ˈɜːli/";
+            soundSentence = "/media/Vowels/ɜː/The bird heard a word.mp3";
+            example1 = "Bird - /bɜːd/";
+            soundExample1 = "/media/Vowels/ɜː/bird.mp3";
+            example2 = "Word - /wɜːd/";
+            soundExample2 = "/media/Vowels/ɜː/word.mp3";
+            example3 = "Third - /θɜːd/";
+            soundExample3 = "/media/Vowels/ɜː/third.mp3";
+            example4 = "Learn - /lɜːn/";
+            soundExample4 = "/media/Vowels/ɜː/learn.mp3";
+            example5 = "Nurse - /nɜːs/";
+            soundExample5 = "/media/Vowels/ɜː/nurse.mp3";
+            example6 = "Early - /ˈɜːli/";
+            soundExample6 = "/media/Vowels/ɜː/early.mp3";
 
         }else if (sourceButton == btnMoreInformation7) {
-            symbol = "ɪ";
-            sentence = "She needs a new seat";
-            example1 = "Big";
-            example2 = "Fish";
-            example3 = "Sit";
-            example4 = "Milk";
-            example5 = "Tip";
-            example6 = "Hill";
+            symbol = "ə";
+            soundSymbol="/media/Vowels/Vocal7.mp3";
+            sentence = "The teacher is in the cinema";
+            soundSentence = "/media/Vowels/ə/The teacher is in the cinema.mp3";
+            example1 = "Sofa - /ˈsəʊfə/";
+            soundExample1 = "/media/Vowels/ə/sofa.mp3";
+            example2 = "Banana - /bəˈnænə/";
+            soundExample2 = "/media/Vowels/ə/banana.mp3";
+            example3 = "About - /əˈbaʊt/";
+            soundExample3 = "/media/Vowels/ə/about.mp3";
+            example4 = "Camera - /ˈkæmrə/";
+            soundExample4 = "/media/Vowels/ə/camera.mp3";
+            example5 = "Elephant - /ˈɛlɪfənt/";
+            soundExample5 = "/media/Vowels/ə/elephant.mp3";
+            example6 = "Doctor - /ˈdɒktə/";
+            soundExample6 = "/media/Vowels/ə/doctor.mp3";
 
         }else if (sourceButton == btnMoreInformation8) {
-            symbol = "iː";
-            sentence = "She needs a new seat";
-            example1 = "Feet";
-            example2 = "Eat";
-            example3 = "Leave";
-            example4 = "Green";
-            example5 = "Tree";
-            example6 = "See";
+            symbol = "ɔː";
+            soundSymbol="/media/Vowels/Vocal8.mp3";
+            sentence = "He bought more food";
+            soundSentence = "/media/Vowels/ɔː/He bought more food.mp3";
+            example1 = "Door - /dɔː/";
+            soundExample1 = "/media/Vowels/ɔː/door.mp3";
+            example2 = "Floor - /flɔː/";
+            soundExample2 = "/media/Vowels/ɔː/floor.mp3";
+            example3 = "Law - /lɔː/";
+            soundExample3 = "/media/Vowels/ɔː/law.mp3";
+            example4 = "More - /mɔː/";
+            soundExample4 = "/media/Vowels/ɔː/more.mp3";
+            example5 = "Saw - /sɔː/";
+            soundExample5 = "/media/Vowels/ɔː/saw.mp3";
+            example6 = "Call - /kɔːl/";
+            soundExample6 = "/media/Vowels/ɔː/call.mp3";
 
         }else if (sourceButton == btnMoreInformation9) {
-            symbol = "iː";
-            sentence = "She needs a new seat";
-            example1 = "Feet";
-            example2 = "Eat";
-            example3 = "Leave";
-            example4 = "Green";
-            example5 = "Tree";
-            example6 = "See";
+            symbol = "æ";
+            soundSymbol="/media/Vowels/Vocal9.mp3";
+            sentence = "A cat sat on a black mat";
+            soundSentence = "/media/Vowels/æ/A cat sat on a black mat.mp3";
+            example1 = "Cat - /kæt/";
+            soundExample1 = "/media/Vowels/æ/cat.mp3";
+            example2 = "Hat - /hæt/";
+            soundExample2 = "/media/Vowels/æ/hat.mp3";
+            example3 = "Black - /blæk/";
+            soundExample3 = "/media/Vowels/æ/black.mp3";
+            example4 = "Apple - /ˈæpəl/";
+            soundExample4 = "/media/Vowels/æ/apple.mp3";
+            example5 = "Bat - /bæt/";
+            soundExample5 = "/media/Vowels/æ/bat.mp3";
+            example6 = "Sad - /sæd/";
+            soundExample6 = "/media/Vowels/æ/sad.mp3";
 
         }else if (sourceButton == btnMoreInformation10) {
-            symbol = "iː";
-            sentence = "She needs a new seat";
-            example1 = "Feet";
-            example2 = "Eat";
-            example3 = "Leave";
-            example4 = "Green";
-            example5 = "Tree";
-            example6 = "See";
+            symbol = "ʌ";
+            soundSymbol="/media/Vowels/Vocal10.mp3";
+            sentence = "The sun is hot";
+            soundSentence = "/media/Vowels/ʌ/The sun is hot.mp3";
+            example1 = "Cup - /kʌp/";
+            soundExample1 = "/media/Vowels/ʌ/cup.mp3";
+            example2 = "Love - /lʌv/";
+            soundExample2 = "/media/Vowels/ʌ/love.mp3";
+            example3 = "Sun - /sʌn/";
+            soundExample3 = "/media/Vowels/ʌ/sun.mp3";
+            example4 = "Run - /rʌn/";
+            soundExample4 = "/media/Vowels/ʌ/run.mp3";
+            example5 = "Fun - /fʌn/";
+            soundExample5 = "/media/Vowels/ʌ/fun.mp3";
+            example6 = "Mud - /mʌd/";
+            soundExample6 = "/media/Vowels/ʌ/mud.mp3";
 
         }else if (sourceButton == btnMoreInformation11) {
-            symbol = "iː";
-            sentence = "She needs a new seat";
-            example1 = "Feet";
-            example2 = "Eat";
-            example3 = "Leave";
-            example4 = "Green";
-            example5 = "Tree";
-            example6 = "See";
+            symbol = "ɒ";
+            soundSymbol="/media/Vowels/Vocal11.mp3";
+            sentence = "Tom got a new job";
+            soundSentence = "/media/Vowels/ɒ/Tom got a new job.mp3";
+            example1 = "Dog - /dɒɡ/";
+            soundExample1 = "/media/Vowels/ɒ/dog.mp3";
+            example2 = "Log - /lɒɡ/";
+            soundExample2 = "/media/Vowels/ɒ/log.mp3";
+            example3 = "Hot - /hɒt/";
+            soundExample3 = "/media/Vowels/ɒ/hot.mp3";
+            example4 = "Fog - /fɒɡ/";
+            soundExample4 = "/media/Vowels/ɒ/fog.mp3";
+            example5 = "Pot - /pɒt/";
+            soundExample5 = "/media/Vowels/ɒ/pot.mp3";
+            example6 = "Boss - /bɒs/";
+            soundExample6 = "/media/Vowels/ɒ/boss.mp3";
 
         }else if (sourceButton == btnMoreInformation12) {
-            symbol = "iː";
-            sentence = "She needs a new seat";
-            example1 = "Feet";
-            example2 = "Eat";
-            example3 = "Leave";
-            example4 = "Green";
-            example5 = "Tree";
-            example6 = "See";
+            symbol = "ɑː";
+            soundSymbol="/media/Vowels/Vocal12.mp3";
+            sentence = "Mark’s car is very fast";
+            soundSentence = "/media/Vowels/ɑː/Mark’s car is very fast.mp3";
+            example1 = "Car - /kɑː/";
+            soundExample1 = "/media/Vowels/ɑː/car.mp3";
+            example2 = "Park - /pɑːk/";
+            soundExample2 = "/media/Vowels/ɑː/park.mp3";
+            example3 = "Hard - /hɑːd/";
+            soundExample3 = "/media/Vowels/ɑː/hard.mp3";
+            example4 = "Dark - /dɑːk/";
+            soundExample4 = "/media/Vowels/ɑː/dark.mp3";
+            example5 = "Start - /stɑːt/";
+            soundExample5 = "/media/Vowels/ɑː/start.mp3";
+            example6 = "Far - /fɑː/";
+            soundExample6 = "/media/Vowels/ɑː/far.mp3";
 
         }
 
-        abrirVentanaInformacion(sentence, example1, example2, example3, example4, example5, example6, symbol, mouseEvent);
+        abrirVentanaInformacion(sentence, example1, example2, example3, example4, example5, example6, symbol,
+                soundSymbol, soundSentence, soundExample1, soundExample2, soundExample3, soundExample4, soundExample5, soundExample6,selectedUrlPage , mouseEvent);
 
     }
 
     private void abrirVentanaInformacion(
-            String sentence, String example1, String example2, String example3,
-            String example4, String example5, String example6, String symbol, MouseEvent event) {
+            String sentence, String example1, String example2, String example3, String example4, String example5, String example6, String symbol,
+            String selectedSoundSymbol,String selectedSoundSentence, String selectedSoundExample1, String selectedSoundExample2, String selectedSoundExample3, String selectedSoundExample4, String selectedSoundExample5, String selectedSoundExample6, String urlPage,
+            MouseEvent event) {
 
         try {
             FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(Main.class.getResource("information-example-ipa.fxml")));
@@ -234,11 +340,33 @@ public class VowelsMonophthongsController{
 
             // Obtener el controlador y pasar la información
             InformationExampleIpaController controller = loader.getController();
-            controller.setInformation(sentence, example1, example2, example3, example4, example5, example6, symbol);
+            controller.setInformation(
+                    sentence,
+                    example1,
+                    example2,
+                    example3,
+                    example4,
+                    example5,
+                    example6,
+                    symbol);
 
             // Obtener el Stage actual y cerrarlo
             Stage currentStage = (Stage) ((Button) event.getSource()).getScene().getWindow();
             currentStage.close();
+
+            // Pasar las rutas de los archivos de audio
+            controller.setAudioFiles(
+                    selectedSoundSymbol,
+                    selectedSoundSentence,
+                    selectedSoundExample1,
+                    selectedSoundExample2,
+                    selectedSoundExample3,
+                    selectedSoundExample4,
+                    selectedSoundExample5,
+                    selectedSoundExample6,
+                    urlPage
+
+            );
 
             // Mostrar la nueva ventana
             Stage stage = new Stage();
